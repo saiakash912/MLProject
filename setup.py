6e3eb2e46e520b6f7f9afad380ca2349a60c0404
Venkata Guru Sai Akash Kollati <venkatagurusaiakashkollati@Venkatas-MacBook-Air.local>
2024-05-13 19:01:31 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Akash',
+author_email='saiakash540@gmail.com',
+packages=find_packages()
+install_requires=['pandas','numpy','seaborn']
+)
